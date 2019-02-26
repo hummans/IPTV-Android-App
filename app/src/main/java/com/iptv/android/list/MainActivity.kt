0d383af5ca633afe8faa938da37bd9d8d8df97c0
tcbaras <Turkcell123>
2019-02-27 01:18:29 +0300

    Search has been implemented.

--- app/src/main/java/com/iptv/android/list/MainActivity.kt
+++ app/src/main/java/com/iptv/android/list/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.iptv.android.list
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.SearchView
 import android.util.Log
+import android.view.Menu
 import com.iptv.android.R
 import com.iptv.android.player.PlayerExo
 import com.muparse.M3UItem
@@ -13,6 +17,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val adapter = PlaylistAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +26,9 @@ class MainActivity : AppCompatActivity() {
         rvPlayList.setHasFixedSize(true)
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         rvPlayList.layoutManager = layoutManager
-        rvPlayList.adapter = PlaylistAdapter().apply {
+        rvPlayList.adapter = adapter
+
+        adapter.apply {
             data = m3UPlaylist
             listener = object : PlaylistAdapter.PlayItemSelectListener {
                 override fun onPlayItemSelected(m3UItem: M3UItem) {
@@ -32,12 +39,38 @@ class MainActivity : AppCompatActivity() {
                         Log.d(TAG, "$m3UItem selected")
                         startActivity(intent)
                     }
-
                 }
             }
         }
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
 
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu?.findItem(R.id.action_search)?.actionView as SearchView
+        searchView.setSearchableInfo(
+            searchManager
+                .getSearchableInfo(componentName)
+        )
+        searchView.maxWidth = Integer.MAX_VALUE
+
+        // listening to search query text change
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // filter recycler view when query submitted
+                adapter.filter.filter(query)
+                return false
+            }
+
+            override fun onQueryTextChange(query: String): Boolean {
+                // filter recycler view when text is changed
+                adapter.filter.filter(query)
+                return false
+            }
+        })
+        return true
     }
 
     companion object {
--- app/src/main/java/com/iptv/android/list/PlaylistAdapter.kt
+++ app/src/main/java/com/iptv/android/list/PlaylistAdapter.kt
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.TextView
 import com.amulyakhare.textdrawable.TextDrawable
@@ -17,20 +19,24 @@ import com.muparse.M3UItem
 import kotlinx.android.synthetic.main.item_playlist.view.*
 import kotlin.properties.Delegates
 
-class PlaylistAdapter : RecyclerView.Adapter<PlaylistAdapter.ViewHolder>() {
+class PlaylistAdapter : RecyclerView.Adapter<PlaylistAdapter.ViewHolder>(), Filterable {
 
     internal var data: List<M3UItem> by Delegates.observable(emptyList()) { _, _, _ ->
+        filteredData.clear()
+        filteredData.addAll(data)
         notifyDataSetChanged()
     }
+
+    private var filteredData: MutableList<M3UItem> = mutableListOf()
 
     internal var listener: PlayItemSelectListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_playlist, parent, false))
 
-    override fun getItemCount() = data.size
+    override fun getItemCount() = filteredData.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(data[position], listener)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(filteredData[position], listener)
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val icon: ImageView = itemView.iv_channel
@@ -48,6 +54,40 @@ class PlaylistAdapter : RecyclerView.Adapter<PlaylistAdapter.ViewHolder>() {
             itemView.setOnClickListener { listener?.let { listener.onPlayItemSelected(model) } }
         }
     }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(query: CharSequence?): FilterResults {
+                if (query.isNullOrEmpty()) {
+                    filteredData.clear()
+                    filteredData.addAll(data)
+                } else {
+                    filteredData.clear()
+                    for (m3U in data) {
+                        m3U.itemName?.let {
+                            if (it.contains(query, ignoreCase = true)) {
+                                filteredData.add(m3U)
+                            }
+                        }
+                    }
+                }
+
+                val filterResults = FilterResults()
+                filterResults.values = filteredData
+                return filterResults
+            }
+
+            override fun publishResults(query: CharSequence?, filterResults: FilterResults?) {
+                try {
+                    filteredData = filterResults?.values as ArrayList<M3UItem>
+                    notifyDataSetChanged()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
 
     interface PlayItemSelectListener {
         fun onPlayItemSelected(m3UItem: M3UItem)
