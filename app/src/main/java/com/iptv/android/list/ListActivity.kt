201eff47e4a3e12f4959d342a8de158fa1825c60
tcbaras <Turkcell123>
2019-03-15 20:59:27 +0300

    casting integration initiation failed.

--- app/src/main/java/com/iptv/android/list/ListActivity.kt
+++ app/src/main/java/com/iptv/android/list/ListActivity.kt
@@ -9,19 +9,39 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.SearchView
 import android.util.Log
 import android.view.Menu
+import com.google.android.gms.cast.framework.CastButtonFactory
 import com.iptv.android.R
 import com.iptv.android.player.PlayerExo
 import com.muparse.M3UItem
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.cast.framework.CastContext
+import com.google.android.gms.cast.CastRemoteDisplayLocalService
+import android.support.v7.media.MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY
+import android.support.v4.app.NotificationCompat.getExtras
+import com.google.android.gms.cast.CastDevice
+import com.google.android.gms.cast.CastMediaControlIntent
+import android.support.v7.media.MediaRouteSelector
+import android.support.v7.media.MediaRouter
+import android.widget.Toast
+import android.app.PendingIntent
+import android.net.wifi.WifiConfiguration
+import com.google.android.gms.common.api.Status
 
 
 class ListActivity : AppCompatActivity() {
 
+    private val mediaRouter: MediaRouter? = null
+    private var castDevice: CastDevice? = null
+
     private val adapter = PlaylistAdapter()
+
+    private var mCastContext: CastContext? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mCastContext = CastContext.getSharedInstance(this)
 
         rvPlayList.setHasFixedSize(true)
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
@@ -42,10 +62,20 @@ class ListActivity : AppCompatActivity() {
                 }
             }
         }
+
+
+        setupMediaRouter()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
         menuInflater.inflate(R.menu.main_menu, menu)
+
+        CastButtonFactory.setUpMediaRouteButton(
+            applicationContext,
+            menu,
+            R.id.media_route_menu_item
+        )
 
         // Associate searchable configuration with the SearchView
         val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
@@ -71,6 +101,114 @@ class ListActivity : AppCompatActivity() {
             }
         })
         return true
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        if (!isRemoteDisplaying()) {
+            if (castDevice != null) {
+                startCastService(castDevice)
+            }
+        }
+    }
+
+    public override fun onDestroy() {
+        if (mediaRouter != null) {
+            mediaRouter.removeCallback(mMediaRouterCallback)
+        }
+        super.onDestroy()
+    }
+
+
+    private fun setupMediaRouter() {
+        mediaRouter = MediaRouter.getInstance(applicationContext)
+        val mediaRouteSelector = MediaRouteSelector.Builder().addControlCategory(
+            CastMediaControlIntent.categoryForCast(getString(R.string.app_cast_id))
+        ).build()
+        if (isRemoteDisplaying()) {
+            this.castDevice = CastDevice.getFromBundle(mediaRouter.getSelectedRoute().extras)
+        } else {
+            val extras = intent.extras
+            if (extras != null) {
+                castDevice = extras.getParcelable(INTENT_EXTRA_CAST_DEVICE)
+            }
+        }
+
+        mediaRouter.addCallback(
+            mediaRouteSelector, mMediaRouterCallback,
+            MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY
+        )
+    }
+
+    private fun isRemoteDisplaying(): Boolean {
+        return CastRemoteDisplayLocalService.getInstance() != null
+    }
+
+
+    private val mMediaRouterCallback = object : MediaRouter.Callback() {
+        override fun onRouteSelected(router: MediaRouter?, info: MediaRouter.RouteInfo) {
+            castDevice = CastDevice.getFromBundle(info.extras)
+            Toast.makeText(
+                applicationContext,
+                getString(R.string.app_id) + info.name, Toast.LENGTH_LONG
+            ).show()
+            startCastService(castDevice)
+        }
+
+        override fun onRouteUnselected(router: MediaRouter?, info: MediaRouter.RouteInfo?) {
+            if (isRemoteDisplaying()) {
+                CastRemoteDisplayLocalService.stopService()
+            }
+            castDevice = null
+        }
+    }
+
+    private fun startCastService(castDevice: CastDevice) {
+        val intent = Intent(this@ListActivity, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+        val notificationPendingIntent = PendingIntent.getActivity(this@ListActivity, 0, intent, 0)
+
+        val settings = CastRemoteDisplayLocalService.NotificationSettings.Builder().setNotificationPendingIntent(
+            notificationPendingIntent
+        ).build()
+
+        CastRemoteDisplayLocalService.startService(this@ListActivity, PresentationService::class.java!!,
+            getString(R.string.app_id), castDevice, settings,
+            object : CastRemoteDisplayLocalService.Callbacks {
+                override fun onRemoteDisplaySessionError(p0: Status?) {
+                    initError()
+
+                    this@ListActivity.castDevice = null
+                    this@ListActivity.finish()
+                }
+
+                override fun onServiceCreated(service: CastRemoteDisplayLocalService) {
+                    (service as PresentationService).setAdViewModel(
+                        fragmentStatePagerAdapter.getAdAt(currentPosition)
+                    )
+                }
+
+                override fun onRemoteDisplaySessionStarted(service: CastRemoteDisplayLocalService) {}
+
+                fun onRemoteDisplaySessionError(errorReason: WifiConfiguration.Status) {
+                    initError()
+
+                    this@ListActivity.castDevice = null
+                    this@ListActivity.finish()
+                }
+
+                override fun onRemoteDisplaySessionEnded(castRemoteDisplayLocalService: CastRemoteDisplayLocalService) {}
+            })
+    }
+
+    private fun initError() {
+        val toast = Toast.makeText(
+            applicationContext, R.string.toast_connection_error,
+            Toast.LENGTH_SHORT
+        )
+        mediaRouter?.selectRoute(mediaRouter.defaultRoute)
+        toast.show()
     }
 
     companion object {
--- app/src/main/java/com/iptv/android/player/PlayerExo.java
+++ app/src/main/java/com/iptv/android/player/PlayerExo.java
@@ -3,13 +3,25 @@ package com.iptv.android.player;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.MediaRouteButton;
+import android.util.Log;
 import android.view.WindowManager;
 import com.devbrackets.android.exomedia.ui.widget.VideoView;
+import com.google.android.gms.cast.MediaInfo;
+import com.google.android.gms.cast.framework.*;
+import com.google.android.gms.cast.framework.media.RemoteMediaClient;
 import com.iptv.android.R;
 
 public class PlayerExo extends AppCompatActivity implements com.devbrackets.android.exomedia.listener.OnPreparedListener {
 
+    private static final String TAG = "BURHAN";
     VideoView videoView;
+    private String url = "";
+
+    private CastSession mCastSession;
+    private SessionManager mSessionManager;
+    private final SessionManagerListener mSessionManagerListener = new SessionManagerListenerImpl();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,17 +31,103 @@ public class PlayerExo extends AppCompatActivity implements com.devbrackets.andr
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Bundle bundle = getIntent().getExtras();
 
+        mSessionManager = CastContext.getSharedInstance(this).getSessionManager();
+        MediaRouteButton mMediaRouteButton = findViewById(R.id.media_route_button);
+        CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), mMediaRouteButton);
+        CastContext castContext = CastContext.getSharedInstance(this);
+
         videoView = findViewById(R.id.video_view);
         videoView.setOnPreparedListener(this);
         try {
+            url = bundle.getString("Url");
             videoView.setVideoURI(Uri.parse(bundle.getString("Url")));
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
     }
 
     @Override
     public void onPrepared() {
         videoView.start();
+    }
+
+    @Override
+    protected void onResume() {
+        mCastSession = mSessionManager.getCurrentCastSession();
+        mSessionManager.addSessionManagerListener(mSessionManagerListener);
+        super.onResume();
+
+
+        MediaInfo mediaInfo = new MediaInfo.Builder(url)
+                .setStreamType(MediaInfo.STREAM_TYPE_BUFFERED)
+                .setContentType("videos/mp4")
+//                .setMetadata(movieMetadata)
+//                .setStreamDuration(mSelectedMedia.getDuration() * 1000)
+                .build();
+        if (mCastSession != null) {
+            RemoteMediaClient remoteMediaClient = mCastSession.getRemoteMediaClient();
+            remoteMediaClient.load(mediaInfo);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSessionManager.removeSessionManagerListener(mSessionManagerListener);
+        mCastSession = null;
+    }
+
+
+    private class SessionManagerListenerImpl implements SessionManagerListener {
+        @Override
+        public void onSessionStarting(Session session) {
+
+        }
+
+        @Override
+        public void onSessionStarted(Session session, String sessionId) {
+            Log.d(TAG, "onSessionStarted() called with: session = [" + session + "], sessionId = [" + sessionId + "]");
+            invalidateOptionsMenu();
+        }
+
+        @Override
+        public void onSessionStartFailed(Session session, int i) {
+            Log.d(TAG, "onSessionStartFailed() called with: session = [" + session + "], i = [" + i + "]");
+        }
+
+        @Override
+        public void onSessionEnding(Session session) {
+            Log.d(TAG, "onSessionEnding() called with: session = [" + session + "]");
+        }
+
+        @Override
+        public void onSessionResumed(Session session, boolean wasSuspended) {
+            Log.d(TAG, "onSessionResumed() called with: session = [" + session + "], wasSuspended = [" + wasSuspended + "]");
+            invalidateOptionsMenu();
+        }
+
+        @Override
+        public void onSessionResumeFailed(Session session, int i) {
+            Log.d(TAG, "onSessionResumeFailed() called with: session = [" + session + "], i = [" + i + "]");
+        }
+
+        @Override
+        public void onSessionSuspended(Session session, int i) {
+            Log.d(TAG, "onSessionSuspended() called with: session = [" + session + "], i = [" + i + "]");
+        }
+
+        @Override
+        public void onSessionEnded(Session session, int error) {
+            Log.d(TAG, "onSessionEnded() called with: session = [" + session + "], error = [" + error + "]");
+            finish();
+        }
+
+        @Override
+        public void onSessionResuming(Session session, String s) {
+            Log.d(TAG, "onSessionResuming() called with: session = [" + session + "], s = [" + s + "]");
+        }
     }
 }
